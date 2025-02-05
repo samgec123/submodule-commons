3630e0c7e09f22b1a1fbfc2ae86b9d8dcd09cf30
samgec123 <sam.gec@gmail.com>
2025-02-05 21:55:17 +0530

    Update cards.js

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -25,5 +25,5 @@ export default function decorate(block) {
   block.append(ul);
   //adding using feature-card from nexa.
   //this change is from arena to common repo
-  //change from commons 12345
+  //change from commons 123456
 }
